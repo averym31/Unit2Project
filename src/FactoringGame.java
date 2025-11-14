117139599b418ee554382fc39991ed4653e2efa0
Averym31 <averym31@nycstudents.net>
2025-11-14 08:45:26 -0500

    Nov 14 25

--- src/FactoringGame.java
+++ src/FactoringGame.java
@@ -1,18 +1,43 @@
+import java.util.Scanner;
 public class FactoringGame {
     public static void readRules() throws InterruptedException {
-        System.out.println("Press the enter key to skip:");
-        System.out.println("Welcome to Avery's Factoring Game! Here are the rules:");
-        Thread.sleep(5000);
-        System.out.println("A quadratic equation (a^2+bx+c) will be shown, it is your responsibility to factor out this equation in the (mx-p)(nx-q) format, with you determining both the sign and the values of each variable.");
+        System.out.println("Here are the rules:");
+        Thread.sleep(2000);
+        System.out.println("A quadratic equation (a^2+bx+c) will be shown, it is your responsibility to factor out this equation in a f(qx+r)(sx+t) format, with you determining both the sign and the values of each variable.");
         Thread.sleep(5000);
         System.out.println("Getting it wrong deducts both time and potential points earned, and if you run out of time on one of the questions, your game is over.");
         Thread.sleep(5000);
         System.out.println("The time for every question will gradually get shorter, so using a paper and pencil to write stuff down is highly recommended!");
         Thread.sleep(5000);
         System.out.println("With the rules out of the way, let the game begin!");
+        Thread.sleep(2000);
     }
 
-    public static void playGame() throws InterruptedException {
+    public static int gcfSearch(int firstSide, int secondSide){
+        int a = firstSide;
+        int b = secondSide;
+        if (b > a){
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        int remainder = 1;
+        for (int i = 0; i * (a % b) == 0; i++) {
+            remainder = (a%b);
+            a = b;
+            b = remainder;
+            System.out.println(remainder);
+        }
+        return remainder;
+    }
 
+    public static void quadGenerator() throws InterruptedException {
+
+        int q = (int) (Math.random() * 3) + 1;
+        int r = (int) (Math.random() * 5) + 1;
+        int s = (int) (Math.random() * 3) + 1;
+        int t = (int) (Math.random() * 5) + 1;
+        System.out.println(q + " " + r + " " + s + " " + t);
+        System.out.println("Factor this equation!: " + (q*s) + "x^2+" + ((q*t) + (r*s)) + "x+" + (r*t));
     }
 }
--- src/FactoringGameRunner.java
+++ src/FactoringGameRunner.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class FactoringGameRunner {
     public static void main(String[] args) throws InterruptedException {
-        FactoringGame.readRules();
-        FactoringGame.playGame();
+        Scanner scanner = new Scanner(System.in);
+        // System.out.print("Welcome to Avery's Factoring Game! If you want to read the rules, please type \"yes\". Skip this line if you don't want to read the rules. ");
+        // if (scanner.nextLine().equals("yes")){
+        //  FactoringGame.readRules();
+        //}
+        //else
+        //{
+        //    System.out.println("Alright then, let the game begin!");
+        //    Thread.sleep(1000);
+        //}
+        FactoringGame.quadGenerator();
     }
 }
